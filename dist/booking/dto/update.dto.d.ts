ce4af227def4ffab0311a96369eb25912e8623cc
IcedCrescent <ntn201@gmail.com>
2025-04-12 00:18:12 +0700

    remove dist

--- dist/booking/dto/update.dto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class UpdateBookingDto {
-    status: number;
-}
--- dist/room/dto/create-room.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class CreateRoomDto {
-    name: string;
-    description?: string;
-    status: boolean;
-    room_class_id: number;
-}
